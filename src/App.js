3be49e52fc582dd325121c2a70d85b64ebdac7c2
cfornetti <77817531+cfornetti@users.noreply.github.com>
2021-10-06 22:07:14 -0700

    Created the search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Dictionary from "./Dictionary"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+    <div className="container">
+      <header className="App-header">
+      </header>
+      <main>
+        <Dictionary />
+      </main>
+    <footer className="App-footer">
+      <small>
+      Created by <a href="https://github.com/cfornetti">Chiara Fornetti</a>
+      </small>
+    </footer>
+    </div>
     </div>
   );
 }
